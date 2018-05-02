7c9a8650e4828906ca01be7c23aa1dafbff76f6a
Guille <guillote_gg@hotmail.com>
2018-05-02 12:48:53 -0300

    ser default port to 3000 in case PORT is not set

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const {ObjectID} = require('mongodb');
 const {authenticate} = require('./middleware/authenticate');
 
 var app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
